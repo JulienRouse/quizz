676f132d9379c39311deca1a65fcd8ec3104af50
JulienRouse <julien.rouse@gmail.com>
2016-03-16 17:41:33 +0100

    accessor for the values

--- stats.lisp
+++ stats.lisp
@@ -4,8 +4,28 @@
 
 
 ;;store a list with 4 numbers:
-;;-number of good anwer
+;;-number of good answer
 ;;-number of bad answer
 ;;-number of !pass
 ;;-number of !hint
-(defparameter simple-stat (list 0 0 0 0))
+(defparameter *simple-stats* (list 0 0 0 0))
+
+
+;;(decode-universal-time (get-universal-time)) 
+;;for later use
+
+
+;;four accessors for the four values in *simple-stats*
+(defun get-number-good-answer ()
+  (first *simple-stats*))
+
+(defun get-number-bad-answer ()
+  (second *simple-stats*))
+
+(defun get-number-pass ()
+  (third *simple-stats*))
+
+(defun get-number-hint ()
+  (fourth *simple-stats*))
+
+
