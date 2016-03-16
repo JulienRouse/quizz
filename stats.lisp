3ae0945a2702da1ab0a97e348a0ade082ba6052f
JulienRouse <julien.rouse@gmail.com>
2016-03-16 21:31:11 +0100

    function to read/write a stat file

--- stats.lisp
+++ stats.lisp
@@ -28,4 +28,48 @@
 (defun get-number-hint ()
   (fourth *simple-stats*))
 
+(defun reset-stats ()
+  (setf *simple-stats* (list 0 0 0 0)))
+
+
+(defun read-stats (&key (filename #p"./ressources/stats"))
+  (with-open-file (in filename :direction :input :if-does-not-exist :create)
+    (let ((line (read-line in nil)))
+      (if (null line)
+	  (reset-stats)
+	  (let ((line-split (split-sequence:split-sequence #\; line)))
+	    (setf (first *simple-stats*)  (parse-integer (first line-split)))
+	    (setf (second *simple-stats*) (parse-integer (second line-split)))
+	    (setf (third *simple-stats*)  (parse-integer (third line-split)))
+	    (setf (fourth *simple-stats*) (parse-integer (fourth line-split)))
+	    *simple-stats*)))))
+
+
+(defun write-stats (&key (filename #p"./ressources/stats"))
+  (with-open-file (out filename :direction :output :if-exists :supersede)
+    (format out 
+	    "~A;~A;~A;~A" 
+	    (get-number-good-answer)
+	    (get-number-bad-answer)
+	    (get-number-pass)
+	    (get-number-hint))))
+
+
+;;do i want them with exact ratio or an approximate float?
+(defun %percentage (sum num)
+  (float (/ num sum)))
+
+(defun percentage ()
+  (let ((sum (+ (get-number-good-answer)
+		(get-number-bad-answer)
+		(get-number-pass)
+		(get-number-hint))))
+    (values (%percentage sum (get-number-good-answer))
+	    (%percentage sum (get-number-bad-answer))
+	    (%percentage sum (get-number-pass))
+	    (%percentage sum (get-number-hint)))))
+
+
+
+
 
